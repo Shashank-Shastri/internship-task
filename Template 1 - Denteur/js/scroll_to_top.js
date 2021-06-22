6cd35e80d399de697225e9042f42b6ec95d0e889
Shashank-Shastri <shashank.m.shastri@gmail.com>
2021-06-22 13:30:33 +0530

    Style Formatting

--- Template 1 - Denteur/js/scroll_to_top.js
+++ Template 1 - Denteur/js/scroll_to_top.js
@@ -4,17 +4,16 @@ const offset_opacity = 1200;
 const scroll_top_duration = 700;
 const $back_to_top = $('.cd-top');
 
-$(window).scroll(function(){
-    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-    if( $(this).scrollTop() > offset_opacity ) { 
+$(window).scroll(function() {
+    ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if ($(this).scrollTop() > offset_opacity) {
         $back_to_top.addClass('cd-fade-out');
     }
 });
 
-$back_to_top.on('click', function(event){
+$back_to_top.on('click', function(event) {
     event.preventDefault();
     $('body,html').animate({
-        scrollTop: 0 ,
-        }, scroll_top_duration
-    );
+        scrollTop: 0,
+    }, scroll_top_duration);
 });
